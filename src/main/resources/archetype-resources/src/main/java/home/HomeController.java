bd2c0c49162ce6778a5e721259ddd130ce4bdce5
Stanley Wang <stanley.wang@momentussoftware.com>
2014-01-27 11:38:26 -0800

    log annotation

--- src/main/resources/archetype-resources/src/main/java/home/HomeController.java
+++ src/main/resources/archetype-resources/src/main/java/home/HomeController.java
@@ -2,14 +2,23 @@ package ${package}.home;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import ${package}.log.Log;
+
 
 @Controller
 public class HomeController {
 	
+	@Log
+	private Logger log;
+
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Principal principal) {
+		log.debug("homeController");
 		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
 	}
 }
---
+++ src/main/resources/archetype-resources/src/main/java/log/LoggerPostProcessor.java
@@ -0,0 +1,38 @@
+package ${package}.log;
+
+import java.lang.reflect.Field;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.util.ReflectionUtils.FieldCallback;
+
+public class LoggerPostProcessor implements BeanPostProcessor {
+	 
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws
+        BeansException {
+        return bean;
+    }
+ 
+    public Object postProcessBeforeInitialization(final Object bean, String beanName)
+          throws BeansException {
+        ReflectionUtils.doWithFields(bean.getClass(), new FieldCallback() {
+                @SuppressWarnings("unchecked")
+                public void doWith(Field field) throws IllegalArgumentException,
+                    IllegalAccessException {
+                    ReflectionUtils.makeAccessible(field);
+ 
+                    //Check if the field is annoted with @Log
+                    if (field.getAnnotation(Log.class) != null) {
+                        Log logAnnotation = field.getAnnotation(Log.class);
+                        Logger logger = LoggerFactory.getLogger(bean.getClass());
+                        field.set(bean, logger);
+                    }
+                }
+        });
+ 
+        return bean;
+    }
+}
